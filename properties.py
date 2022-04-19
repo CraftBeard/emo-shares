20dd6d6f8779f0c2624e405b524cb1fa3276bbfa
Alex Liang <lianglx_alex@163.com>
2022-04-19 22:56:48 +0800

    update header

--- properties.py
+++ properties.py
@@ -1,11 +1,11 @@
 # different headers
 HEADERS = [
     {
-        'coding': 'gzip, deflate, br'
-        'accept-language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7'
-        'content-length': '0'
-        'content-type': 'text/plain'
-        'referer': 'https://www.google.com/'
+        'coding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7',
+        'content-length': '0',
+        'content-type': 'text/plain',
+        'referer': 'https://www.google.com/',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36'
     }
 ]
